00bc290b9199be5526c7da2e5bce86957eae32d3
Subhra1505 <subhrajeetganguly0615@gmail.com>
2026-04-22 19:48:10 +0530

    Update and rename Student.java to LibraryOperations.java

---
+++ Project-Student Management System/LibraryOperations.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class LibraryOperations {
+    private Library library;
+    private Scanner sc;
+
+    public LibraryOperations() {
+        library = new Library();
+        sc = new Scanner(System.in);
+    }
+
+    public void start() {
+        int choice;
+
+        do {
+            System.out.println("\n--- Library Menu ---");
+            System.out.println("1. Add Book");
+            System.out.println("2. Remove Book");
+            System.out.println("3. Search Book");
+            System.out.println("4. Display Books");
+            System.out.println("5. Exit");
+            System.out.print("Enter choice: ");
+
+            choice = sc.nextInt();
+            sc.nextLine(); // clear buffer
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Enter Title: ");
+                    String title = sc.nextLine();
+
+                    System.out.print("Enter Author: ");
+                    String author = sc.nextLine();
+
+                    library.addBook(new Book(id, title, author));
+                    break;
+
+                case 2:
+                    System.out.print("Enter Book ID to remove: ");
+                    int removeId = sc.nextInt();
+                    library.removeBook(removeId);
+                    break;
+
+                case 3:
+                    System.out.print("Enter Title to search: ");
+                    String searchTitle = sc.nextLine();
+                    library.searchBook(searchTitle);
+                    break;
+
+                case 4:
+                    library.displayBooks();
+                    break;
+
+                case 5:
+                    System.out.println("Exiting...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice!");
+            }
+
+        } while (choice != 5);
+    }
+}
